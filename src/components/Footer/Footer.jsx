6af3b657fab2ea155ac65759ba4c2b364f0912de
calebchris000 <calebchris000@gmail.com>
2023-04-29 17:13:47 +0100

    Move the footer component to its separate file. Add Round and Goal functionalities

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import { useSelector } from "react-redux";
+
+
+const Footer = () => {
+  const { rounds, goals } = useSelector((store) => store.Pomodoro);
+  return (
+    <footer className="bg-violet-lighter m-auto max-w-xl text-center text-lg font-semibold text-violet-bg flex justify-between p-8 rounded-t-xl fixed bottom-0 left-0 right-0">
+      <div className="">
+        <h3 className="text-2xl ">ROUND</h3>
+        <p className="">
+          <span className="text-3xl mr-1">{rounds}</span>
+          <span className="font-normal text-1xl">/4</span>
+        </p>
+      </div>
+      <span className="border border-1 border-violet-bg"></span>
+      <div>
+        <h3 className="text-2xl">GOAL</h3>
+        <p className="">
+          <span className="text-3xl mr-1">{goals}</span>
+          <span className="font-normal text-1xl">/4</span>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
