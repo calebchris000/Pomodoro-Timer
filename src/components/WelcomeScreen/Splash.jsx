e8d339565a388a039973fd14fa628694e32691b2
calebchris000 <calebchris000@gmail.com>
2023-04-30 20:40:43 +0100

    Remove excess text on splash

--- src/components/WelcomeScreen/Splash.jsx
+++ src/components/WelcomeScreen/Splash.jsx
@@ -15,8 +15,11 @@ const Splash = () => {
 
   if (displaySplash) {
     return (
-      <section ref={splashRef} className="fixed transition-all opacity-100 flex-wrap flex flex-col items-center z-20 top-0 left-0 right-0 bottom-0 bg-violet-bg">
-        <h2 className="relative top-36 text-center text-6xl font-[poppins] text-violet-lighter font-bold">POMODORO TIMER</h2>
+      <section
+        ref={splashRef}
+        className="fixed transition-all opacity-100 flex-wrap flex flex-col items-center z-20 top-0 left-0 right-0 bottom-0 bg-violet-bg"
+      >
+        <h2 className="relative top-36 text-center text-8xl font-[poppins] text-violet-lighter font-semibold">POMO</h2>
         <i className="text-violet-lighter relative top-36 mt-10 text-5xl">
           <FontAwesomeIcon icon={faClock} />
         </i>
--- test.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-
-let item;
-async function musics() {
-    const get = await fs.promises.readdir('./public/audio')
-    console.log(get);
-}
-
-musics()
