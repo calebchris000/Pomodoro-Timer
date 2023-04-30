41f2d850339e462754bf66ba728ced3019c75a11
calebchris000 <calebchris000@gmail.com>
2023-04-30 13:27:45 +0100

    Add poppins as font

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,9 @@ import { useSelector } from "react-redux";
 const Footer = () => {
   const { rounds, goals } = useSelector((store) => store.Pomodoro);
   return (
-    <footer className="bg-violet-lighter m-auto max-w-xl text-center text-lg font-semibold text-violet-bg flex justify-between p-8 rounded-t-xl fixed bottom-0 left-0 right-0">
+    <footer className="bg-violet-lighter m-auto max-w-xl text-center text-lg font-semibold text-violet-bg flex justify-between p-8 rounded-t-xl font-[poppins] fixed bottom-0 left-0 right-0">
       <div className="">
-        <h3 className="text-2xl ">ROUND</h3>
+        <h3 className="text-2xl">ROUND</h3>
         <p className="">
           <span className="text-3xl mr-1">{rounds}</span>
           <span className="font-normal text-1xl">/4</span>
