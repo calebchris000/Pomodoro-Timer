a291d6e2887f097ce2718bb82b108554f2dbc2da
Caleb Nwaizu <calebchris000@gmail.com>
2023-11-03 15:32:50 +0100

    Move file to UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
   import Pomodoro from './app/UI/Pomodoro.svelte';
 </script>
 
